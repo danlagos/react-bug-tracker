c141bcbdb7c9ad2feacc27a5b234108f11b625b3
Daniel Lagos <lagos.d@gmail.com>
2020-01-22 20:02:31 -0800

    first commit.

---
+++ src/Tickets/Tickets.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const tickets = (props) => {
+    return (
+        <div>
+            <h1>These are tickets</h1>
+            <form>
+                <label for="name">Name</label>
+                <input type="text" name="name" placeholder="First Name"></input>
+                <input type="text"
+
+
+
+
+
+                {/* <label for ></label>
+                <label for="details">Details</label><br></br>
+                <input type="text" name="details" size="75" style={{height: "170px"}}></input> */}
+            </form>
+        </div>
+    )
+};
+
+
+export default tickets
